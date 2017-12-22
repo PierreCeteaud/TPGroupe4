d684c5b53737a352874524643cb6671291889a3f
Pierre C <pierre.ceteaud@laposte.net>
2017-12-22 10:52:26 +0100

    Création des images
    Extraction des images depuis le mp4

--- Extraction images.py
+++ Extraction images.py
@@ -7,13 +7,19 @@ Created on Tue Dec 19 20:00:09 2017
 
 import cv2
 import matplotlib.pyplot as plt
-cap = cv2.VideoCapture('06-11-22.mp4')
+import os
 
-# Attention on affiche toutes les images du film
+
+
+cap = cv2.VideoCapture('06-11-22.mp4')
+compresion_params=[cv2.IMWRITE_PNG_COMPRESSION,9];    
+NumImage=0
 while(cap.isOpened()):
     ret, frame = cap.read()
-    plt.imshow(cv2.cvtColor(frame,cv2.COLOR_BGR2RGB))
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
+    NumImage+=1
+    #plt.imshow(cv2.cvtColor(frame,cv2.COLOR_BGR2RGB))
+    # Permet de reprendre l'enregistrement là où on était
+    Name=f"06-11-22-{NumImage}.png"
+    if not os.path.isfile(Name):
+        cv2.imwrite(Name,frame, compresion_params)    
 cap.release()

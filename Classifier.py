e9c982382a5f240705c2dee30c3d3ff1a947941c
Pierre C <pierre.ceteaud@laposte.net>
2018-01-21 16:11:14 +0100

    4 approches

--- Classifier.py
+++ Classifier.py
@@ -5,6 +5,8 @@ Created on Mon Jan  1 23:41:27 2018
 @author: Pierre
 """
 import numpy as np
+import matplotlib.pyplot as plt
+import itertools
 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.ensemble import RandomForestClassifier
@@ -83,6 +85,43 @@ def Print(Result):
               "Rappel :",f"{round(R[1][0]/(R[1][0]+R[1][2])*100,1)}%",
               "Erreur :",f"{round((R[1][1]+R[1][2])/sum(R[1])*100,1)}%")
 
+def plot_confusion_matrix(cm,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    classes=['N','V','A','VA']
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+"""
+Result=Liste_Resultats[0]
+"""
     #print(1-((PredTrain==Audio_Y).sum()/Audio_Y.size),1-((PredTest==Audio_TY).sum()/Audio_TY.size))
     
 def PredictionFenetres(NumSequence,Reel,Predictions):    
--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import Classifier
 import Fenetrage
 import numpy as np
 import importlib
+import sys
 
-OnlyClassifieur2=True
 
 ## Lecture du fichier des séquences
 dateReference=datetime(1900,1,1)
@@ -24,16 +24,17 @@ with open("Présente.csv",'r') as FSequences:
     for line in FSequences:       
         texte=line.split(";")        
         try:
-            Candidat=[datetime.strptime(texte[0],"%M:%S,%f")-dateReference,
-                  datetime.strptime(texte[1],"%M:%S,%f")-dateReference,
+            Candidat=[datetime.strptime(texte[0],"%H:%M:%S,%f")-dateReference,
+                  datetime.strptime(texte[1],"%H:%M:%S,%f")-dateReference,
                   int(texte[2]), # Présentatrice parle
                   int(texte[3]), # Présentatrice à l'écran
                   int(texte[4]), # Changement de séquence dû à l'audio
                   int(texte[5]), # Changement de séquence dû à la vidéo                  
                   ]
             Ok=True
-        except: 
+        except : 
         # Si erreur au décodage (1ère et dernière ligne) on ignore
+            print(sys.exc_info())
             Ok=False
         if Ok :
             # On rajoute une colonne avec la durée de la séquence
@@ -54,6 +55,10 @@ Train, Test = train_test_split(Sequences, train_size=ratio, random_state = 42)
 importlib.reload(Fenetrage)
 NumSeqTrain,Audio_Y,Video_Y,FenetresTrain=Fenetrage.Decoupe(Train,0.5,1)
 NumSeqTest,Audio_TY,Video_TY,FenetresTest=Fenetrage.Decoupe(Test,0.5,1)
+Audio_Y_tmp=np.asarray(Audio_Y)
+Audio_Y0=Audio_Y_tmp[Audio_Y_tmp==0]
+Audio_Y1=Audio_Y_tmp[Audio_Y_tmp==1]
+
 
 importlib.reload(Audio)
 Audio_Features=Audio.Features_Audio(FenetresTrain,1,0.5,center=False)
@@ -64,11 +69,8 @@ Audio_Test_Features=Audio.Features_Audio(FenetresTest,1,0.5,center=False)
 Video_Test_Features=Video.Features_Video(FenetresTest,1,cadree=True)
 
 # Concaténation et normalisation des features audio et Video
-
 Features=np.hstack((Audio_Features,Video_Features))
 TestFeatures=np.hstack((Audio_Test_Features,Video_Test_Features))
-
-importlib.reload(Classifier)
 
 Normalisation=True
 if Normalisation:
@@ -79,50 +81,66 @@ if Normalisation:
 Both_Y=Audio_Y*2+Video_Y
 Both_TY=Audio_TY*2+Video_TY
 # Classification globale
-if not OnlyClassifieur2:
-    G=Classifier.LDA(Features, Both_Y,TestFeatures,Both_TY,(0,1,2,3))
-    print("Un classifieur")
-    print("Taux d'erreur sur le train:",1-(G[0][0]+G[0][5]+G[0][10]+G[0][15])/sum(G[0]))
-    print("Taux d'erreur sur le test:",1-(G[1][0]+G[1][5]+G[1][10]+G[1][15])/sum(G[1]))
+G=Classifier.LDA(Features, Both_Y,TestFeatures,Both_TY,(0,1,2,3))
+print("Un classifieur")
+print("Taux d'erreur sur le train:",1-(G[0][0]+G[0][5]+G[0][10]+G[0][15])/sum(G[0]))
+print("Taux d'erreur sur le test:",1-(G[1][0]+G[1][5]+G[1][10]+G[1][15])/sum(G[1]))
 
-    # C'est moins bien :-) => on garde les entrées videos pour améliorer l
-    Y11=(Audio_Y==1)&(Video_Y==1)
-    Y10=(Audio_Y==1)&(Video_Y==0)
-    Y01=(Audio_Y==0)&(Video_Y==1)
-    Y00=(Audio_Y==0)&(Video_Y==0)
-    TY11=(Audio_TY==1)&(Video_TY==1)
-    TY10=(Audio_TY==1)&(Video_TY==0)
-    TY01=(Audio_TY==0)&(Video_TY==1)
-    TY00=(Audio_TY==0)&(Video_TY==0)
+LabelsA=[]
+suf=""
+for d in range(3):
+    for i in range(Audio.n_mfcc):
+        LabelsA.append("mfcc"+suf+str(i))
+    suf=suf+"'"
+LabelsA.append("ZCR")
+LabelsA.append("Spectral contrats")
+LabelsV=[]
+for i in ("gris","bleu","vert","rouge","saturation","luminosité","teinte"):
+    for j in ("moyenne", "variance"):
+        LabelsV.append(j+" "+i)
+Labels.append("Dynamisme")
+for i in ("bleu","teinte","vert","saturation","rouge","luminosité"):
+    for j in range (16):
+        LabelsV.append("Histogramme "+i+" "+str(j))
+LabelsAV=LabelsA+LabelsV
+
+Importance1=list(zip(LabelsAV,abs(G[3].coef_.T)))
+fichier = open("Importance1.txt", "w")
+fichier.write("Feature\tN\tA\tV\tAV\n")
+for ligne in Importance1:
+    fichier.write(f"{ligne[0]}\t{ligne[1][0]}\t{ligne[1][1]}\t{ligne[1][2]}\t{ligne[1][3]}\n")
+
+fichier.close()
     
-    Liste_Resultats=[]
-    for F in (("Audio+Video",Features,TestFeatures),("Audio",Audio_Features,Audio_Test_Features),("Video",Video_Features,Video_Test_Features)):
-        for Y in (("Audio",Audio_Y,Audio_TY),
-                  ("Video",Video_Y,Video_TY),
-                  ("Totalement absente",Y00,TY00),
-                  ("Présente video",Y01,TY01),
-                  ("Présente audio",Y10,TY01),
-                  ("Présente Audio+Video",Y11,TY11)):           
-            #print(Y[0],"grâce à",F[0])
-            Result=Classifier.LDA(F[1],Y[1],F[2],Y[2])
-            Liste_Resultats.append(Result)
-            #Classifier.Print(Result)
-else:
-    Liste_Resultats=[]
-    F =("Audio+Video",Features,TestFeatures)
+importlib.reload(Classifier)
+Liste_Resultats=[]
+for F in (("Audio+Video",Features,TestFeatures),("Audio",Audio_Features,Audio_Test_Features),("Video",Video_Features,Video_Test_Features)):
     for Y in (("Audio",Audio_Y,Audio_TY),
-              ("Video",Video_Y,Video_TY)):
-        Result=Classifier.LDA(F[1],Y[1],F[2],Y[2])
+              ("Video",Video_Y,Video_TY)):           
+              #("Totalement absente",Y00,TY00),
+              #("Présente video",Y01,TY01),
+              #("Présente audio",Y10,TY01),
+              #("Présente Audio+Video",Y11,TY11)
+        #print(Y[0],"grâce à",F[0])
+        Result=Classifier.LR(F[1],Y[1],F[2],Y[2])
         Liste_Resultats.append(Result)
-        #Classifier.Print(Result)
-"""
-np.argwhere(np.isnan(Features))
-np.argwhere(np.isnan(Video_Features))
-np.argwhere(np.isnan(Audio_Features))
-Classifier.Mad_[84]
-Classifier.Mediane_[84]
-Classifier.NormaliseTrain(Features)[:,84:88]
-"""
+        if F=="Audio+Video":
+            Importance=list(zip(LabelsAV,abs(Result[3].coef_.T)))
+        elif F=="Audio":
+            Importance=list(zip(LabelsA,abs(Result[3].coef_.T)))
+        else:
+            Importance=list(zip(LabelsV,abs(Result[3].coef_.T)))
+        fichier = open(f"Importance_{F[0]}={Y[0]}.txt", "w")
+        fichier.write("Feature\tImportance\n")
+        for ligne in Importance:
+            fichier.write(f"{ligne[0]}\t{ligne[1]}\n")
+        fichier.close()
+
+            #Classifier.Print(Result)
+
+
+
+
 # Nombre d'erreurs avec deux classifeurs
 print("Deux classifieurs")        
 print("Taux d'erreur sur le train:",1-((Audio_Y==Liste_Resultats[0][2][0])
@@ -131,99 +149,67 @@ print("Taux d'erreur sur le train:",1-((Audio_Y==Liste_Resultats[0][2][0])
 print("Taux d'erreur sur le test:",1-((Audio_TY==Liste_Resultats[0][2][1])
                                     &(Video_TY==Liste_Resultats[1][2][1])).sum()/len(Audio_TY))
 
-Labels=[]
-suf=""
-for d in range(3):
-    for i in range(Audio.n_mfcc):
-        Labels.append("mfcc"+suf+str(i))
-    suf=suf+"'"
-Labels.append("ZCR")
-Labels.append("Spectral contrats")
-for i in ("gris","bleu","vert","rouge","saturation","luminosité","teinte"):
-    for j in ("moyenne", "variance"):
-        Labels.append(j+" "+i)
-Labels.append("Dynamisme")
-for i in ("bleu","teinte","vert","saturation","rouge","luminosité"):
-    for j in range (16):
-        Labels.append("Histogramme "+i+" "+str(j))
+
+# Matrices de confusion
+from sklearn.metrics import confusion_matrix
+print ("1ère approche")
+CM=confusion_matrix(Both_TY,G[2][1])
+Classifier.plot_confusion_matrix(CM)
+plt.show()
+Classifier.plot_confusion_matrix(CM,True)
+plt.show()
+
+print ("2ème approche")
+Aggregation=Liste_Resultats[0][2][1]*2+Liste_Resultats[1][2][1]
+CM=confusion_matrix(Both_TY,Aggregation)
+Classifier.plot_confusion_matrix(CM)
+plt.show()
+Classifier.plot_confusion_matrix(CM,True)
+plt.show()
+
+print ("3ème approche")
+Aggregation=Liste_Resultats[2][2][1]*2+Liste_Resultats[5][2][1]
+CM=confusion_matrix(Both_TY,Aggregation)
+Classifier.plot_confusion_matrix(CM)
+plt.show()
+Classifier.plot_confusion_matrix(CM,True)
+plt.show()
+
+print ("4ème approche")
+A_A=Liste_Resultats[2]
+A_V=Liste_Resultats[3]
+V_A=Liste_Resultats[4]
+V_V=Liste_Resultats[5]
+PoidsInterne=2
+Seuil=(PoidsInterne+1)/2
+RAudio=(A_A[3].predict(Audio_Test_Features)*PoidsInterne+V_A[3].predict(Video_Test_Features))>Seuil
+RVideo=(V_V[3].predict(Video_Test_Features)*PoidsInterne+A_V[3].predict(Audio_Test_Features))>Seuil
+Aggregation=RAudio*2+RVideo
+CM=confusion_matrix(Both_TY,Aggregation)
+Classifier.plot_confusion_matrix(CM)
+plt.show()
+Classifier.plot_confusion_matrix(CM,True)
+plt.show()
+
         
-ImportanceAudio=list(zip(Labels,abs(Liste_Resultats[0][3].coef_).reshape(len(Labels))))
-ImportanceVideo=list(zip(Labels,abs(Liste_Resultats[1][3].coef_).reshape(len(Labels))))
+ImportanceAudio=list(zip(Labels,(Liste_Resultats[0][3].feature_importances_).reshape(len(Labels))))
+ImportanceAudio=list(zip(Labels,(abs(Liste_Resultats[0][3].coef_)).reshape(len(Labels))))
+ImportanceVideo=list(zip(Labels,(abs(Liste_Resultats[1][3].scalings_*Liste_Resultats[1][3].coef_.T)).reshape(len(Labels))))
+ScalingsAudio=list(zip(Labels,(abs(Liste_Resultats[0][3].scalings_)).reshape(len(Labels))))
+ImportanceAudio=list(zip(Labels,(Liste_Resultats[0][3].explained_variance_ratio_).reshape(len(Labels))))
+ImportanceVideo=list(zip(Labels,(abs(Liste_Resultats[1][3].scalings_*Liste_Resultats[1][3].coef_.T)).reshape(len(Labels))))
+abs(Liste_Resultats[0][3].scalings_*Liste_Resultats[0][3].coef_[0,0])
 
 len(Labels)
 Audio_Features.shape
 Video_Features.shape
 Liste_Resultats[0][3].coef_.shape
 np.median(Features,axis=0)
+np.mean(Features,axis=0)
 from statsmodels import robust
 robust.mad(Features,axis=0)
 
 
-if not OnlyClassifieur2:
-    # Nombre d'erreurs avec quatre classifeurs
-    print("Quatre classifieurs")
-    TrainPredit00=Liste_Resultats[2][2][0].astype(int)
-    Test_Predit00=Liste_Resultats[2][2][1].astype(int)
-    TrainPredit0V=Liste_Resultats[3][2][0].astype(int)
-    Test_Predit0V=Liste_Resultats[3][2][1].astype(int)
-    TrainPreditA0=Liste_Resultats[4][2][0].astype(int)
-    Test_PreditA0=Liste_Resultats[4][2][1].astype(int)
-    TrainPreditAV=Liste_Resultats[5][2][0].astype(int)
-    Test_PreditAV=Liste_Resultats[5][2][1].astype(int)
-    
-    TrainPredit=(TrainPredit00+TrainPredit0V+TrainPreditA0+TrainPreditAV)==1
-    TrainOk=(TrainPredit&((TrainPredit00&Y00)|(TrainPredit0V&Y01)|(TrainPreditA0&Y10)|(TrainPreditAV&Y11))).sum()
-    Test_Predit=(Test_Predit00+Test_Predit0V+Test_PreditA0+Test_PreditAV)==1
-    Test_Ok=(Test_Predit&((Test_Predit00&TY00)|(Test_Predit0V&TY01)|(Test_PreditA0&TY10)|(Test_PreditAV&TY11))).sum()
-    
-    print("Train : % avec une prédiction",TrainPredit.sum()/len(TrainPredit))
-    print("Train : % erreur sur prédiction",1-TrainOk/TrainPredit.sum())
-    print("Train : % erreur sur l'ensemble",1-TrainOk/len(TrainPredit))
-    print("Test  : % avec une prédiction",Test_Predit.sum()/len(Test_Predit))
-    print("Test  : % erreur sur prédiction",1-Test_Ok/Test_Predit.sum())
-    print("Test  : % erreur sur l'ensemble",1-Test_Ok/len(Test_Predit))
-    
-    print("Fenetres :")
-    
-    print("Un classifieur")
-    Pred_Train=G[2][0]
-    Pred_Test=G[2][1]
-    
-    Classe=Audio_Y*2+Video_Y # 0= absente A et V, 1 présente vidéo, 2 présente Audio, 3 présente audio et vidéo
-    PredictionsFenetres,Ok,Ko=Classifier.PredictionFenetres(NumSeqTrain,Classe,Pred_Train)
-    print("Taux d'erreur sur le train :",Ko/(Ok+Ko))
-    
-    Classe=Audio_TY*2+Video_TY # 0= absente A et V, 1 présente vidéo, 2 présente Audio, 3 présente audio et vidéo
-    PredictionsFenetres,Ok,Ko=Classifier.PredictionFenetres(NumSeqTest,Classe,Pred_Test)
-    print("Taux d'erreur sur le test :",Ko/(Ok+Ko))
-    
-    print("Deux classifieurs")
-    Pred_Train=Liste_Resultats[0][2][0]*2+Liste_Resultats[1][2][0]
-    Pred_Test=Liste_Resultats[0][2][1]*2+Liste_Resultats[1][2][1]
-    
-    Classe=Audio_Y*2+Video_Y # 0= absente A et V, 1 présente vidéo, 2 présente Audio, 3 présente audio et vidéo
-    PredictionsFenetres,Ok,Ko=Classifier.PredictionFenetres(NumSeqTrain,Classe,Pred_Train)
-    print("Taux d'erreur sur le train :",Ko/(Ok+Ko))
-    
-    #importlib.reload(Classifier)
-    Classe=Audio_TY*2+Video_TY # 0= absente A et V, 1 présente vidéo, 2 présente Audio, 3 présente audio et vidéo
-    PredictionsFenetres,Ok,Ko=Classifier.PredictionFenetres(NumSeqTest,Classe,Pred_Test)
-    print("Taux d'erreur sur le test :",Ko/(Ok+Ko))
-    
-    print("Quatre classifieurs")
-    
-    Classe=Audio_Y*2+Video_Y # 0= absente A et V, 1 présente vidéo, 2 présente Audio, 3 présente audio et vidéo
-    Predictions=list(zip(TrainPredit00,TrainPredit0V,TrainPreditA0,TrainPreditAV))
-    
-    PredictionsFenetres,Ok,Ko=Classifier.PredictionFenetre4Classifieurs(NumSeqTrain,Classe,Predictions)
-    
-    print("Taux d'erreur sur le train :",Ko/(Ok+Ko))
-    Classe=Audio_TY*2+Video_TY # 0= absente A et V, 1 présente vidéo, 2 présente Audio, 3 présente audio et vidéo
-    Predictions=list(zip(Test_Predit00,Test_Predit0V,Test_PreditA0,Test_PreditAV))
-    
-    PredictionsFenetres,Ok,Ko=Classifier.PredictionFenetre4Classifieurs(NumSeqTest,Classe,Predictions)
-    
-    print("Taux d'erreur sur le test :",Ko/(Ok+Ko))
     
     """
     
